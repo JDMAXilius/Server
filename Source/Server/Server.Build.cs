df9b9346f66c71dc0e8581c8d0099aa1dc415b58
JDMAXinius <jdlugorivera@student.fullsail.edu>
2024-09-16 08:44:26 -0400

    Docker Setup

--- Source/Server/Server.Build.cs
+++ Source/Server/Server.Build.cs
@@ -8,6 +8,6 @@ public class Server : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core","UMG", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemSteam", "OnlineSubsystemUtils"/*, "OnlineSubsystemEOS" , "OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemBlueprints","Networking", "GameplayTasks", "PlayFab", "PlayFabCommon", "PlayFabCpp"*/ });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core","UMG", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem",/* "OnlineSubsystemSteam",*/ "OnlineSubsystemUtils"/*, "OnlineSubsystemEOS" , "OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemBlueprints","Networking", "GameplayTasks", "PlayFab", "PlayFabCommon", "PlayFabCpp"*/ });
 	}
 }
